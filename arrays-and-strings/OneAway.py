d82b42333e460cb551f12e46b6a2973d40ae237c
prash <dprashureddy007@gmail.com>
2020-07-06 13:19:24 -0700

    oneaway

--- arrays-and-strings/OneAway.py
+++ arrays-and-strings/OneAway.py
@@ -2,18 +2,29 @@ def one_away(str1, str2):
     if abs(len(str1) - len(str2)) > 1:
         return False
 
-    if len(str1) < len(str2):
-        str1, str2 = str2, str1
+    if abs(len(str1) - len(str2)) == 1:
+        edits = 0
+        idx = 0
+        for char in str1:
+            if str2[idx] != char:
+                edits += 1
+                if edits == 1:
+                    idx -= 1
+            idx += 1
+    elif len(str1) == len(str2):
+        edits = 0
+        idx = 0
+        for char in str1:
+            if str2[idx] != char:
+                edits += 1
+            idx += 1
     
-    edits = 0
-    idx = 0
-    for char in str1:
-        if str2[idx] != char:
-            edits += 1
-            idx -= 1
-        
-        idx += 1
+    if edits > 1:
+        return False
+    return True
         
 
-str1 = "pale"
-str2 = "ple"
+str1 = "brle"
+str2 = "frfe"
+
+print(one_away(str1, str2))
